1e76194b8b979ce95403dd9e1350df3288e025fc
eshimovTK <eshimov_tk@mail.ru>
2024-11-28 11:24:03 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,5 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { program } from 'commander';
 
-const program = new Command();
-
-program.option('-f, --foo', 'enable some foo');
-
-program.addHelpText(
-  'after',
-  `
-
-Example call:
-  $ custom-help --help`,
-);
-
-program.parse(process.argv);
+program.parse();
