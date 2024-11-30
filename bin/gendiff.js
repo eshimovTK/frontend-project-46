e6ce93b421dcef7c6682edc21f93594158ff6bcc
timur <eshimov_tk@mail.ru>
2024-11-30 21:21:56 +0300

    update gendiff, npm fs, add file1,2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.0');
+  .version('0.0.0')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
