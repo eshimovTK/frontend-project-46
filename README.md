9c4d9308b48a85995ac01965258fda1364d1f839
timur <eshimov_tk@mail.ru>
2025-01-26 17:30:32 +0300

    mod README.md, bin/gendiff.js, add Makefile

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/eshimovTK/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/eshimovTK/frontend-project-46/actions)
+<a href="https://codeclimate.com/github/eshimovTK/frontend-project-46/maintainability"><img src="https://api.codeclimate.com/v1/badges/7a0ce411fd5b578ba466/maintainability" /></a>
+<a href="https://codeclimate.com/github/eshimovTK/frontend-project-46/test_coverage"><img src="https://api.codeclimate.com/v1/badges/7a0ce411fd5b578ba466/test_coverage" /></a>
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,35 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import _ from 'lodash';
 import parser from '../parser.js';
+
+const gendiff = (file1, file2) => {
+  const obj1 = { ...file1 };
+  const obj2 = { ...file2 };
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const keysSort = _.uniq(keys1.concat(keys2)).sort();
+  console.log('{');
+  for (const key of keysSort) {
+    if (obj2[key] === undefined) {
+      console.log(`  - ${key}: ${obj1[key]}`);
+      continue;
+    }
+    if (obj1[key] === obj2[key]) {
+      console.log(`    ${key}: ${obj1[key]}`);
+      continue;
+    }
+    if (obj2[key] !== obj1[key] && obj1[key] !== undefined) {
+      console.log(`  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`);
+      continue;
+    }
+    if (obj2[key] !== obj1[key] && obj1[key] === undefined) {
+      console.log(`  + ${key}: ${obj2[key]}`);
+    }
+  }
+  console.log('}');
+};
 
 program
   .description('Compares two configuration files and shows a difference.')
@@ -9,9 +37,9 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const obj1 = parser(filepath1);
-    const obj2 = parser(filepath2);
-    console.log(obj1, obj2);
+    const obj1 = JSON.parse(parser(filepath1));
+    const obj2 = JSON.parse(parser(filepath2));
+    gendiff(obj1, obj2);
   });
 
 program.parse();
